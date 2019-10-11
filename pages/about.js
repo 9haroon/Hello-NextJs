b6eee99d8a15258a697b507c75ffff5a6376ce4a
Kittisak Malaisanan <moonrinnn@gmail.com>
2019-10-11 16:35:48 +0700

    Hello Next

---
+++ pages/about.js
@@ -0,0 +1,9 @@
+import Layout from "../components/Layout";
+
+const content = <p>This is the about page</p>;
+
+export default function About() {
+  	return (
+    	<Layout content={content}/>
+  	);
+}
